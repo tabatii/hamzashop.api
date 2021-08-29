6599e4dccd530cc5699ab123ce5dd9e19235fccf
tabatii <76856660+tabatii@users.noreply.github.com>
2021-08-29 15:26:27 +0100

    initial commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,7 +10,8 @@ class ProductController extends Controller
 {
     public function index()
     {
-        //
+        $products = Product::all();
+        return response()->json(new ProductResource($products));
     }
 
     public function store(Request $request)
