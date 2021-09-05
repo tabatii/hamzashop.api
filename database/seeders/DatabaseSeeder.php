9123deb6054755358790fd0db01d0f154567c060
tabatii <76856660+tabatii@users.noreply.github.com>
2021-09-05 20:57:51 +0100

    initial commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Admin::factory(1)->create();
+        $this->call([
+            AdminSeeder::class
+        ]);
     }
 }
