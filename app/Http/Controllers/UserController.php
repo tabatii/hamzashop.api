f782a40629f4cf00e50af7d30432c4fa455911cc
tabatii <76856660+tabatii@users.noreply.github.com>
2021-09-13 17:14:57 +0100

    initial commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -37,7 +37,7 @@ class UserController extends Controller
     public function destroy($id)
     {
         $user = User::findOrFail($id);
-        $user->delete();
+        $user->forceDelete();
         return response()->json();
     }
 }
